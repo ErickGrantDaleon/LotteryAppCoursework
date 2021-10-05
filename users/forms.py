e7bf76d736bd1d4a292e20313095ffcbc1ccdcc9
c0021401 <c0021401@newcastle.ac.uk>
2021-10-05 01:56:38 +0100

    Add Required validator
    Imported from wtforms.validators.

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required
 
+# TODO: Email must be a valid email address.
+# TODO: Firstname and Lastname must not contain the following characters:  * ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < >
+# TODO: Phone must be of the form XXXX-XXX-XXXX
+# TODO: Password must be between 6 and 12 characters in length.
+# TODO: Password must contain at least 1 digit, 1 lowercase, 1 uppercase and 1 special character.
+# TODO: Password and Confirm Password must match.
+# TODO: PIN Key must be exactly 32 characters in length.
+# TODO: Relevant validation error messages must be shown.
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
+    email = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required()])
+    lastname = StringField(validators=[Required()])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required()])
+    confirm_password = PasswordField(validators=[Required()])
+    pin_key = StringField(validators=[Required()])
     submit = SubmitField()
