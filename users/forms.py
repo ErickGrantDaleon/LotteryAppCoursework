1f0c24235acb2d40046081dec143dafb334f133a
c0021401 <c0021401@newcastle.ac.uk>
2021-10-05 03:13:39 +0100

    Add validator to check for invalid phone number
    Created function that check for if phone number is in the format XXXX-XXX-XXXX. Also Updated register.html to notify user of invalid phone number.

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email, ValidationError
+from wtforms.validators import Required, Email, ValidationError, Length
 
-# TODO: Phone must be of the form XXXX-XXX-XXXX
 # TODO: Password must be between 6 and 12 characters in length.
 # TODO: Password must contain at least 1 digit, 1 lowercase, 1 uppercase and 1 special character.
 # TODO: Password and Confirm Password must match.
@@ -13,14 +12,20 @@ def character_check(form,field):
     excluded_chars = "*?!'^+%&/()=}][{$#@<>"
     for char in field.data:
         if char in excluded_chars:
-            raise ValidationError(
-                f"Character {char} is not allowed.")
+            raise ValidationError(f"Character {char} is not allowed.")
+
+def phone_check(form,field):
+    if len(field.data) != 13:
+        raise ValidationError("Phone must be of the form XXXX-XXX-XXXX.")
+    for i, char in enumerate(field.data):
+        if not ((i in {4, 8} and char == "-") or char.isnumeric()):
+            raise ValidationError("Phone must be of the form XXXX-XXX-XXXX.")
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     firstname = StringField(validators=[Required(), character_check])
     lastname = StringField(validators=[Required(), character_check])
-    phone = StringField(validators=[Required()])
+    phone = StringField(validators=[Required(), phone_check])
     password = PasswordField(validators=[Required()])
     confirm_password = PasswordField(validators=[Required()])
     pin_key = StringField(validators=[Required()])
