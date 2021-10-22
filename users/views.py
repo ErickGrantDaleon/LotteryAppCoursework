e2c4ba49021dfc3f5c79b92a623e780633b06fe6
c0021401 <c0021401@newcastle.ac.uk>
2021-10-22 22:06:36 +0100

    Account, profile and logout tabs require logging in to access

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask import Blueprint, render_template, flash, redirect, url_for, request, session
 from flask_login import current_user
 from werkzeug.security import check_password_hash
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 from app import db
 from models import User
 from users.forms import RegisterForm, LoginForm
@@ -98,6 +98,7 @@ def login():
 
 
 @users_blueprint.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
@@ -105,12 +106,14 @@ def logout():
 
 # view user profile
 @users_blueprint.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html', name="PLACEHOLDER FOR FIRSTNAME")
 
 
 # view user account
 @users_blueprint.route('/account')
+@login_required
 def account():
     return render_template('account.html',
                            acc_no="PLACEHOLDER FOR USER ID",
