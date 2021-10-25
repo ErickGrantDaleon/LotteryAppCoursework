5e970766b2eb7c102725bd6a0767fe27ba818362
c0021401 <c0021401@newcastle.ac.uk>
2021-10-25 12:57:17 +0100

    Logging no longer duplicates entries

--- app.py
+++ app.py
@@ -8,21 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 # TODO: FIX DUPLICATE LOGGING ENTRIES
-# logging
-class SecurityFilter(logging.Filter):
-    def filter(self, record):
-        return "SECURITY" in record.getMessage()
 
-
-fh = logging.FileHandler('lottery.log', mode='a')
-fh.setLevel(logging.WARNING)
-fh.addFilter(SecurityFilter())
-formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
-fh.setFormatter(formatter)
-
-logger = logging.getLogger('')
-logger.propagate = False
-logger.addHandler(fh)
 
 # config
 app = Flask(__name__)
@@ -74,6 +60,21 @@ if __name__ == "__main__":
     free_socket.listen(5)
     free_port = free_socket.getsockname()[1]
     free_socket.close()
+
+    # logging
+    class SecurityFilter(logging.Filter):
+        def filter(self, record):
+            return "SECURITY" in record.getMessage()
+
+    fh = logging.FileHandler('lottery.log', mode='a')
+    fh.setLevel(logging.WARNING)
+    fh.addFilter(SecurityFilter())
+    formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
+    fh.setFormatter(formatter)
+
+    logger = logging.getLogger('')
+    logger.propagate = False
+    logger.addHandler(fh)
 
     # generates login manager
     login_manager = LoginManager()
